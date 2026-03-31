edb2c265864e559bcf727391b4b89db9a49db3a2
Cristopher Martins <cristopher.martins@glinttglobal.com>
2026-03-31 16:48:33 +0100

    fix KC_SLSH

--- keyboards/bastardkb/charybdis/4x6/keymaps/cristopher/keymap.c
+++ keyboards/bastardkb/charybdis/4x6/keymaps/cristopher/keymap.c
@@ -65,7 +65,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   // ├───────────────────────────────────────────────────────────────┤             ├──────────────────────────────────────────────────────┤
         KC_TAB,   LSFT_T(KC_A), LCTL_T(KC_S), LALT_T(KC_D), LGUI_T(KC_F), KC_G,    KC_H,     RGUI_T(KC_J), RALT_T(KC_K), RCTL_T(KC_L), RSFT_T(KC_SCLN), KC_QUOT,
   // ├───────────────────────────────────────────────────────────────┤              ├──────────────────────────────────────────────────────┤
-        KC_GRAVE,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_FSLH,  KC_BSLS,
+        KC_GRAVE,  KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,                     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,  KC_BSLS,
   // ╰───────────────────────────────────────────────────────────────┤              ├──────────────────────────────────────────────────────╯
                                        LT(LAYER_MEDIA, KC_ESC),  KC_SPC,   RAISE,                     RAISE,    KC_ENT,
                                                  KC_DEL,   LOWER,                     LOWER
